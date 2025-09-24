cba7c7b1468fd22b44976ca946fc300181f022f7
kaushal kumar jha <jhakaushal361@gmail.com>
2025-09-25 04:22:47 +0530

    first commit

---
+++ App-main/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './app.css'
+import Home from './components/Home'
+
+const App = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4'>
+      <div className="text-center font-bold">
+        <h1 className='text-5xl font-bold text-gray-800 mb-8'>AI Image Enhancer{" "}</h1>
+        <p className='text-lg text-gray-500'>Upload your Image and let AI enhance to in seconds</p>
+      </div>
+      <Home/>
+
+      <div className="text-lg text-gray-500">Powered By @AIDeveloper</div>
+      
+
+    </div>
+  )
+}
+
+export default App
---
+++ App-main/src/components/Loading.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../components/Loading.css'
+
+const Loading = () => {
+  return (
+    // <!-- From Uiverse.io by dexter-st --> 
+<div className="loader-wrapper mt-3">
+  <span className="loader-letter">G</span>
+  <span className="loader-letter">e</span>
+  <span className="loader-letter">n</span>
+  <span className="loader-letter">e</span>
+  <span className="loader-letter">r</span>
+  <span className="loader-letter">a</span>
+  <span className="loader-letter">t</span>
+  <span className="loader-letter">i</span>
+  <span className="loader-letter">n</span>
+  <span className="loader-letter">g</span>
+
+  <div className="loader"></div>
+</div>
+
+  )
+}
+
+export default Loading
